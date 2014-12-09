1b15be870f800b705904286fe097aad438fe994a
Erik Noren <noren.erik@gmail.com>
2014-12-09 00:53:10 -0500

    Initial Commit of ProgressiveDownload and Tests

---
+++ VikingErik.Net.Http.ProgressiveDownload/ProgressiveDownload.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace VikingErik.Net.Http
+{
+    public class ProgressiveDownload
+    {
+        public bool IsRangeRequest
+        {
+            get
+            {
+                return _Request.Headers.Range != null &&
+                _Request.Headers.Range.Ranges.Count > 0;
+            }
+        }
+
+        public HttpResponseMessage ResultMessage(Stream stream, string mediaType)
+        {
+            try
+            {
+                if (IsRangeRequest)
+                {
+                    var content = new ByteRangeStreamContent(stream, _Request.Headers.Range, mediaType);
+                    var response = _Request.CreateResponse(HttpStatusCode.PartialContent);
+                    response.Content = content;
+
+                    return response;
+                }
+                else
+                {
+                    var content = new StreamContent(stream);
+                    var response = _Request.CreateResponse(HttpStatusCode.OK);
+                    response.Content = content;
+                    response.Content.Headers.ContentType = MediaTypeHeaderValue.Parse(mediaType);
+
+                    return response;
+                }
+            }
+            catch (InvalidByteRangeException ibr)
+            {
+                return _Request.CreateErrorResponse(ibr);
+            }
+            catch (Exception e)
+            {
+                return _Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+
+        public ProgressiveDownload(HttpRequestMessage request)
+        {
+            _Request = request;
+        }
+
+        HttpRequestMessage _Request;
+    }
+}
---
+++ VikingErik.ProgressiveDownload.Tests/ProgressiveDownloadTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Net.Http;
+
+namespace VikingErik.Net.Http.Tests
+{
+    [TestClass]
+    public class ProgressiveDownloadTests
+    {
+        [TestMethod]
+        public void TestProgressiveDownloadWithoutRangeRequest()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Range = null;
+
+            var ms = new MemoryStream(_DummyData);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var vidStreamer = new ProgressiveDownload(request);
+            using (var result = vidStreamer.ResultMessage(ms, "application/octet-stream"))
+            {
+                Assert.AreEqual(System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/octet-stream"), result.Content.Headers.ContentType);
+                Assert.AreEqual(30, result.Content.Headers.ContentLength);
+                Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public void TestProgressiveDownloadWithRangeRequest()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Range = new System.Net.Http.Headers.RangeHeaderValue(0, 5);
+
+            var ms = new MemoryStream(_DummyData);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var vidStreamer = new ProgressiveDownload(request);
+            using (var result = vidStreamer.ResultMessage(ms, "application/octet-stream"))
+            {
+                Assert.AreEqual(System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/octet-stream"), result.Content.Headers.ContentType);
+                Assert.AreEqual(6, result.Content.Headers.ContentLength);
+                Assert.AreEqual(System.Net.HttpStatusCode.PartialContent, result.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public void TestProgressiveDownloadWithOpenRangeRequest()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Range = new System.Net.Http.Headers.RangeHeaderValue(5, null);
+
+            var ms = new MemoryStream(_DummyData);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var vidStreamer = new ProgressiveDownload(request);
+            using (var result = vidStreamer.ResultMessage(ms, "application/octet-stream"))
+            {
+                Assert.AreEqual(System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/octet-stream"), result.Content.Headers.ContentType);
+                Assert.AreEqual(_DummyData.Length - 5, result.Content.Headers.ContentLength);
+                Assert.AreEqual(System.Net.HttpStatusCode.PartialContent, result.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public void TestProgressiveDownloadWithMultipleRangeRequest()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Range = new System.Net.Http.Headers.RangeHeaderValue(0, 4);
+            request.Headers.Range.Ranges.Add(new System.Net.Http.Headers.RangeItemHeaderValue(10, 14));
+
+            var ms = new MemoryStream(_DummyData);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var vidStreamer = new ProgressiveDownload(request);
+            using (var result = vidStreamer.ResultMessage(ms, "application/octet-stream"))
+            {
+                var multipartStart = "multipart/byteranges; boundary=";
+                var responseContentType = result.Content.Headers.ContentType.ToString().Substring(0, multipartStart.Length);
+
+                var resultContent = result.Content.ReadAsStringAsync().Result;
+
+                Assert.AreEqual(multipartStart, responseContentType);
+                Assert.AreEqual(System.Net.HttpStatusCode.PartialContent, result.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public void TestProgressiveDownloadWithBadRange()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Range = new System.Net.Http.Headers.RangeHeaderValue(50, 100);
+
+            var ms = new MemoryStream(_DummyData);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var vidStreamer = new ProgressiveDownload(request);
+            using (var result = vidStreamer.ResultMessage(ms, "application/octet-stream"))
+            {
+                Assert.AreEqual(System.Net.HttpStatusCode.RequestedRangeNotSatisfiable, result.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public void TestProgressiveDownloadWithBadResource()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Range = null;
+
+            MemoryStream ms = null;
+
+            var vidStreamer = new ProgressiveDownload(request);
+            using (var result = vidStreamer.ResultMessage(ms, "application/octet-stream"))
+            {
+                Assert.AreEqual(System.Net.HttpStatusCode.BadRequest, result.StatusCode);
+            }
+        }
+
+        public ProgressiveDownloadTests()
+        {
+            _DummyData = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        }
+
+        byte[] _DummyData;
+    }
+}
